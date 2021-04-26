6d665965c2e0d561d62f8cb18e46aa7a0131f4d7
zwq <2665977048@qq.com>
2021-04-26 11:33:15 +0800

    Revert "瞎几把改"
    This reverts commit eed046db519f16e991f301a2dd6aad9f38e5ab79.
    
    把master分支revert 回clone时的版本

--- src/demo1/HelloWorld.java
+++ src/demo1/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("helloWorld随便再瞎几把改改");
+		System.out.println("helloWorld随便再改改");
 	}
 
 }
